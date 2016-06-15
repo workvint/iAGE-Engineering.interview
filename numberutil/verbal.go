9a32d1a2f38bf722590ec656dae782efa70468b5
workvint <work.vint@gmail.com>
2016-06-16 03:27:54 +0400

    max, min tests green

--- numberutil/verbal.go
+++ numberutil/verbal.go
@@ -1,6 +1,10 @@
 // Package numberutil contains utility functions for working with numbers.
 package numberutil
 
+import (
+	"errors"
+)
+
 const (
 	minNumber = 0
 	maxNumber = 1000000
@@ -11,5 +15,12 @@ const (
 // Min number is 0
 // Max number is 1000000
 func Verbal(number int) (string, error) {
+	switch {
+	case number < minNumber:
+		return "", errors.New("Min number is 0")
+	case number > maxNumber:
+		return "", errors.New("Max number is 1000000")
+	}
+
 	return "", nil
 }
