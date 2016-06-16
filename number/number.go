7ffce512dcfec6ea79d1609d507c64e6347dd7c0
workvint <work.vint@gmail.com>
2016-06-16 12:05:12 +0500

    added console command

--- number/number.go
+++ number/number.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/workvint/numberutil"
+	"strconv"
 )
 
 func main() {
-	result := numberutil.Verbal(1234567)
-	fmt.Println(result)
+	flag.Parse()
+
+	if number, err := strconv.Atoi(flag.Arg(0)); err == nil {
+		if result, err := numberutil.Verbal(number); err == nil {
+			fmt.Println(result)
+		} else {
+			fmt.Println(err.Error())
+		}
+	} else {
+		fmt.Println("usage: $ number [valid integer]")
+	}
 }
